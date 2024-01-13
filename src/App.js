503aca3a34b9c27190f1db5a6522102bba404204
Hayden Roney <roneyhayden@gmail.com>
2024-01-13 13:00:53 -0600

    lab done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
+import { useState } from "react";
+
 function App() {
+  const [emailAddress, setEmailAddress] = useState("")
+  const [nameOfSignee, setNameOfSignee] = useState("")
+  const [listOfChecks, setListOfChecks] = useState({
+    gaming: false,
+    books: false,
+    food: false
+  })
+  const [newsletterStatus, setNewsletterStatus] = useState(false)
+
+  const updateEmailAddress = (e) => setEmailAddress(e.target.value)
+  const updateSigneeName = (e) => setNameOfSignee(e.target.value)
+  const toggleCheck = (e) => {
+    const name = e.target.name
+    let value = e.target.checked
+    setListOfChecks({
+      ...listOfChecks,
+      [name]: value,
+    })
+  }
+
+  const submitNewsletter = (e) => {
+    e.preventDefault();
+    setNewsletterStatus(true)
+  }
+
   return (
     <main>
-      <h1>Hi, I'm (your name)</h1>
+      <h1>Hi, I'm Hayden </h1>
       <img alt="My profile pic" src="https://via.placeholder.com/350" />
       <h2>About Me</h2>
       <p>
@@ -17,6 +44,75 @@ function App() {
       <div>
         <a href="https://github.com">GitHub</a>
         <a href="https://linkedin.com">LinkedIn</a>
+      </div>
+      <div>
+        <h1>Sign up for our Newsletter!</h1>
+        <form onSubmit={submitNewsletter}>
+          <div>
+            <h3>Contact Info</h3>
+            <label htmlFor="email">Enter email address: </label>
+            <input
+              type="text"
+              value={emailAddress}
+              id="email"
+              placeholder="email address"
+              onChange={updateEmailAddress}
+            />
+            <br />
+            <label htmlFor="name">Enter your name: </label>
+            <input
+              type="text"
+              value={nameOfSignee}
+              id="name"
+              placeholder="your name"
+              onChange={updateSigneeName}
+            />
+          </div>
+          <div>
+            <h3>Add your interests</h3>
+            <input
+              type="checkbox"
+              id="gaming"
+              name="gaming"
+              checked={listOfChecks.gaming}
+              aria-checked={listOfChecks.gaming}
+              onChange={toggleCheck}
+            />
+            <label htmlFor="gaming">Gaming</label>
+            <br />
+            <input
+              type="checkbox"
+              id="books"
+              name="books"
+              checked={listOfChecks.books}
+              aria-checked={listOfChecks.books}
+              onChange={toggleCheck}
+            />
+            <label htmlFor="books">Books</label>
+            <br />
+            <input
+              type="checkbox"
+              id="food"
+              name="food"
+              checked={listOfChecks.food}
+              aria-checked={listOfChecks.food}
+              onChange={toggleCheck}
+            />
+            <label htmlFor="food">Food</label>
+          </div>
+          <button type="submit">Subscribe to Newsletter</button>
+        </form>
+        {newsletterStatus ?
+          <>
+            <h2>Thanks for Subscribing!</h2>
+            {(listOfChecks.gaming || listOfChecks.books || listOfChecks.food) ?
+              <h4>You will be notified of:
+                {listOfChecks.gaming ? ` Gaming${(listOfChecks.books || listOfChecks.food) ? "," : ""}` : null}
+                {listOfChecks.books ? ` Books${(listOfChecks.food) ? "," : ""}` : null}
+                {listOfChecks.food ? `${(listOfChecks.gaming || listOfChecks.books) ? " and" : ""} Food` : null}
+              </h4> : null}
+          </>
+          : null}
       </div>
     </main>
   );
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -2,6 +2,7 @@ import { render, screen } from "@testing-library/react";
 import '@testing-library/jest-dom';
 
 import App from "../App";
+import userEvent from "@testing-library/user-event";
 
 // Portfolio Elements
 test("displays a top-level heading with the text `Hi, I'm _______`", () => {
@@ -67,25 +68,74 @@ test("displays the correct links", () => {
 // Newsletter Form - Initial State
 test("the form includes text inputs for name and email address", () => {
   // your test code here
+  render(<App />)
+
+  expect(screen.getByPlaceholderText(/your name/i)).toBeInTheDocument()
+  expect(screen.getByPlaceholderText(/email address/i)).toBeInTheDocument()
 });
 
 test("the form includes three checkboxes to select areas of interest", () => {
   // your test code here
+  render(<App />)
+
+  const gamingCheck = screen.getByRole("checkbox", { name: /gaming/i })
+  const booksCheck = screen.getByRole("checkbox", { name: /books/i })
+  const foodCheck = screen.getByRole("checkbox", { name: /food/i })
+
+  expect(gamingCheck).toBeInTheDocument();
+  expect(booksCheck).toBeInTheDocument();
+  expect(foodCheck).toBeInTheDocument();
 });
 
 test("the checkboxes are initially unchecked", () => {
   // your test code here
+  render(<App />)
+
+  const gamingCheck = screen.getByRole("checkbox", { name: /gaming/i })
+  const booksCheck = screen.getByRole("checkbox", { name: /books/i })
+  const foodCheck = screen.getByRole("checkbox", { name: /food/i })
+
+  expect(gamingCheck).not.toBeChecked();
+  expect(booksCheck).not.toBeChecked();
+  expect(foodCheck).not.toBeChecked();
 });
 
 // Newsletter Form - Adding Responses
 test("the page shows information the user types into the name and email address form fields", () => {
   // your test code here
+  render(<App />)
+
+  const emailText = screen.getByLabelText(/email address/i)
+  const nameText = screen.getByLabelText(/your name/i)
+
+  userEvent.type(emailText, "example@email.com")
+  userEvent.type(nameText, "john smith")
+
+  expect(emailText).toHaveValue("example@email.com")
+  expect(nameText).toHaveValue("john smith")
 });
 
 test("checked status of checkboxes changes when user clicks them", () => {
   // your test code here
+  render(<App />)
+
+  const gamingCheck = screen.getByRole("checkbox", { name: /gaming/i })
+  const booksCheck = screen.getByRole("checkbox", { name: /books/i })
+  const foodCheck = screen.getByRole("checkbox", { name: /food/i })
+
+  userEvent.click(gamingCheck)
+  expect(gamingCheck).toBeChecked();
+  userEvent.click(booksCheck)
+  expect(booksCheck).toBeChecked();
+  userEvent.click(foodCheck)
+  expect(foodCheck).toBeChecked();
 });
 
 test("a message is displayed when the user clicks the Submit button", () => {
   // your test code here
+  render(<App />)
+
+  userEvent.click(screen.getByRole("button", { name: /Subscribe to Newsletter/i }))
+
+  expect(screen.getByText(/thanks for subscribing/i)).toBeInTheDocument();
 });
